9199226fdf5cfada6d29b6293248ed5d9b4abd74
SaqibArsalan <saqibarsalan1997@gmail.com>
2021-04-25 13:11:53 +0500

    test cases functionality added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from "enzyme";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App test", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+  it("should render the root app component", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 });
